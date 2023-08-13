228c8108d054cba8ca8a82987ec0c1851d1582f3
MilanJanko <miljankovic91@gmail.com>
2023-08-13 22:05:02 +0200

    Added header, total price and logo at the end

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ for filepath in filepaths:
     pdf.set_text_color(80, 80, 80)
     pdf.cell(w=30, h=8, txt=columns[0], border=1)
     pdf.cell(w=70, h=8, txt=columns[1], border=1)
-    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=35, h=8, txt=columns[2], border=1)
     pdf.cell(w=30, h=8, txt=columns[3], border=1)
     pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
 
@@ -41,8 +41,27 @@ for filepath in filepaths:
         pdf.set_text_color(80, 80, 80)
         pdf.cell(w=30, h=8, txt=str(row['product_id']), border=1)
         pdf.cell(w=70, h=8, txt=str(row['product_name']), border=1)
-        pdf.cell(w=30, h=8, txt=str(row['amount_purchased']), border=1)
+        pdf.cell(w=35, h=8, txt=str(row['amount_purchased']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
+
+    # Adding last row with total Invoice price
+    pdf.set_font(family='Times', size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=35, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(df['total_price'].sum()), border=1, ln=1)
+
+    # Add total bill
+    bill = f"Your total price is  {df['total_price'].sum()}$"
+    pdf.set_font(family='Times', size=10, style='B')
+    pdf.cell(w=0, h=8, txt=bill, ln=1)
+
+    # Add logo to look professional :)
+    pdf.set_font(family='Times', size=14, style="B")
+    pdf.cell(w=30, h=8, txt="Pug Society")
+    pdf.image('Akica.jpg', w=10)
 
     pdf.output(f'PDF-Invoices/{filename}.pdf')
